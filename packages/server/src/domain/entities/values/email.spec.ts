50b3c1f2fc09f06c6bd43e366deef4a12d7b71d6
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-08-07 14:39:56 -0300

    feat: address check

--- packages/server/src/domain/entities/values/email.spec.ts
+++ packages/server/src/domain/entities/values/email.spec.ts
@@ -24,4 +24,20 @@ describe('Email Unitary Tests', () => {
     expect(testable.isLeft()).toBeTruthy();
     expect((testable.value as InvalidEmail).message).toEqual('Address is required');
   });
+
+  describe('address validation', () => {
+    const invalidEmails = [
+      'invalid_email',
+      'invalid_email@.test',
+      'invalid email@email.com',
+      'invalid_email@com ',
+      'inválid_êmail@com'
+    ].map((invalidAddress) => Email.create({ value: invalidAddress }));
+
+    invalidEmails.forEach((leftEmail) => {
+      expect(leftEmail.isLeft()).toBeTruthy();
+      expect(leftEmail.value).toBeInstanceOf(InvalidEmail);
+      expect((leftEmail.value as InvalidEmail).message).toEqual('Address is invalid');
+    });
+  });
 });
