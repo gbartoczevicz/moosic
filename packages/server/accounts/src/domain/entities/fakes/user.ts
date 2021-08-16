9b0c54fb8661911c98100b9e6cb950ed37c6f7f6
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-08-16 18:51:47 -0300

    feat: add users fake

---
+++ packages/server/accounts/src/domain/entities/fakes/user.ts
@@ -0,0 +1,11 @@
+import { User } from '@/domain/entities/user';
+import { Email, Password, Phone } from '@/domain/entities/values';
+
+type Props = {
+  name: string | 'username';
+  email: Email;
+  password: Password;
+  phone: Phone;
+};
+
+export const makeUser = (props: Props) => User.create(props);
