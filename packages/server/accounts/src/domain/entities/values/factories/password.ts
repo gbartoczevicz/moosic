ef738596253b88340ffd93a30cad52078d3915c2
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-08-22 16:32:25 -0300

    feat: create password factory
    Story: [ch18]

---
+++ packages/server/accounts/src/domain/entities/values/factories/password.ts
@@ -0,0 +1,27 @@
+import { Password } from '@/domain/entities/values';
+import { HashingProvider } from '@/domain/services/ports';
+
+export type MakePasswordProps = {
+  value: string;
+  toEncode: boolean;
+};
+
+export class PasswordFactory {
+  private readonly hashingProvider: HashingProvider;
+
+  private readonly salt: number;
+
+  public constructor(hashingProvider: HashingProvider, salt: number) {
+    this.hashingProvider = hashingProvider;
+    this.salt = salt;
+  }
+
+  public async make(props: MakePasswordProps) {
+    const value = props.toEncode ? await this.hashingProvider.encode(props.value, this.salt) : props.value;
+
+    return Password.create({
+      value,
+      isHashed: props.toEncode
+    });
+  }
+}
