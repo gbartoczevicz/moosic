1819eb9fc1c4544355a3ab77d532901f7647487b
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-08-26 18:06:08 -0300

    feat: add tspath and styled components
    Story: [ch37]

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Container, Title } from '@/lib/container';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+const App: React.FC = () => (
+  <Container>
+    <Title>Open up App.tsx to start working on your app!</Title>
+    <StatusBar style="auto" />
+  </Container>
+);
+
+export default App;
---
+++ packages/mobile/src/lib/container.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #000;
+  text-align: center;
+  font-size: 16px;
+`;
