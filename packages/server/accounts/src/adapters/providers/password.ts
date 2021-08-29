2dd2039e5be1ca8a6638c1d42eb1948b1eb8dc60
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-08-29 14:29:26 -0300

    fix: password adapter signature
    Story: [ch18]

--- packages/server/accounts/src/adapters/providers/password.ts
+++ packages/server/accounts/src/adapters/providers/password.ts
@@ -2,11 +2,11 @@ import * as bcrypt from 'bcryptjs';
 import { PasswordProvider } from '@/ports/providers';
 
 export class PasswordProviderImpl implements PasswordProvider {
-  encode(valueToEncode: string, salt: number): Promise<string> {
+  public async encode(valueToEncode: string, salt: number): Promise<string> {
     return bcrypt.hash(valueToEncode, salt);
   }
 
-  compare(valueToCompare: string, hash: string): Promise<boolean> {
+  public async compare(valueToCompare: string, hash: string): Promise<boolean> {
     return bcrypt.compare(valueToCompare, hash);
   }
 }
