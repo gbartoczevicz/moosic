772ee1475b39a1dd83a22b90176ea7675d963cfe
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-03 19:17:58 -0300

    fix: phone provider validate
    Story: [ch18]

--- packages/server/accounts/src/adapters/providers/phone.ts
+++ packages/server/accounts/src/adapters/providers/phone.ts
@@ -3,7 +3,7 @@ import { PhoneProvider } from '@/ports/providers';
 
 export class PhoneProviderImpl implements PhoneProvider {
   public validate(value: string): boolean {
-    return Validator.isValidNumberForRegion(value, 'BR');
+    return Validator.isValidNumberForRegion(value ?? '', 'BR');
   }
 
   public sanitize(value: string): string {
