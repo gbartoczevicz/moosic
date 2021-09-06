334a9e0d67ea2b5ce7257b57d75d35ae2a7c133e
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-06 17:00:25 -0300

    feat: add CreateEstablishmentUseCase
    Story: [ch16]

---
+++ packages/server/accounts/src/domain/use-cases/create-establishment.ts
@@ -0,0 +1,68 @@
+import { Either, left } from '@shared/utils';
+import { EstablishmentFactory } from '@/domain/factories';
+import { GetRestaurateurUseCase } from '@/domain/use-cases';
+import { CreateEstablishmentDTO } from '@/domain/use-cases/dtos';
+import { EstablishmentsRepo } from '@/ports/database';
+import { PhoneAlreadyInUse, RestaurateurNotFound } from '@/domain/use-cases/errors';
+import { Establishment } from '@/domain/entities';
+import { InfraError } from '@/ports/errors';
+import { FieldIsRequired, PropsAreRequired } from '@/domain/entities/errors';
+import { InvalidPhonePattern } from '@/domain/factories/errors';
+
+type UseCaseEither = Either<
+  PropsAreRequired | FieldIsRequired | InvalidPhonePattern | InfraError | PhoneAlreadyInUse | RestaurateurNotFound,
+  Establishment
+>;
+
+export class CreateEstablishmentUseCase {
+  private readonly establishmentFactory: EstablishmentFactory;
+
+  private readonly establishmentRepo: EstablishmentsRepo;
+
+  private readonly getRestaurateurUseCase: GetRestaurateurUseCase;
+
+  public constructor(
+    establishmentFactory: EstablishmentFactory,
+    establishmentRepo: EstablishmentsRepo,
+    getRestaurateurUseCase: GetRestaurateurUseCase
+  ) {
+    this.establishmentFactory = establishmentFactory;
+    this.establishmentRepo = establishmentRepo;
+    this.getRestaurateurUseCase = getRestaurateurUseCase;
+  }
+
+  public async execute(dto: CreateEstablishmentDTO): Promise<UseCaseEither> {
+    const establishmentOrError = this.establishmentFactory.make({
+      id: {},
+      name: dto.name,
+      phone: { value: dto.phone },
+      restaurateurId: { value: dto.restaurateurId }
+    });
+
+    if (establishmentOrError.isLeft()) {
+      return left(establishmentOrError.value);
+    }
+
+    const establishment = establishmentOrError.value;
+
+    const establishmentWithSamePhoneOrError = await this.establishmentRepo.findByPhone(establishment.phone);
+
+    if (establishmentWithSamePhoneOrError.isLeft()) {
+      return left(establishmentWithSamePhoneOrError.value);
+    }
+
+    if (establishmentWithSamePhoneOrError.value) {
+      return left(new PhoneAlreadyInUse());
+    }
+
+    const restaurateurOrError = await this.getRestaurateurUseCase.execute({
+      userId: establishment.restaurateurId.value
+    });
+
+    if (restaurateurOrError.isLeft()) {
+      return left(restaurateurOrError.value);
+    }
+
+    return this.establishmentRepo.save(establishment);
+  }
+}
--- packages/server/accounts/src/domain/use-cases/index.ts
+++ packages/server/accounts/src/domain/use-cases/index.ts
@@ -2,3 +2,4 @@ export * from '@/domain/use-cases/create-user';
 export * from '@/domain/use-cases/create-restaurateur';
 export * from '@/domain/use-cases/get-user';
 export * from '@/domain/use-cases/get-restaurateur';
+export * from '@/domain/use-cases/create-establishment';
