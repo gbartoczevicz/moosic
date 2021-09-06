8f9bbd208196c0be49757fa41451143c5cbb20c4
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-06 12:11:19 -0300

    feat: add establishment id and restaurateurId
    Story: [ch16]

--- packages/server/accounts/src/domain/entities/establishment.ts
+++ packages/server/accounts/src/domain/entities/establishment.ts
@@ -1,20 +1,28 @@
 import { Either, left, right } from '@shared/utils';
-import { Phone } from '@/domain/entities/values';
+import { Id, Phone } from '@/domain/entities/values';
 import { FieldIsRequired, PropsAreRequired } from '@/domain/entities/errors';
 
 export type EstablishmentProps = {
+  id: Id;
   name: string;
   phone: Phone;
+  restaurateurId: Id;
 };
 
 export class Establishment {
+  public readonly id: Id;
+
   public readonly name: string;
 
   public readonly phone: Phone;
 
-  private constructor(name: string, phone: Phone) {
+  public readonly restaurateurId: Id;
+
+  private constructor(id: Id, name: string, phone: Phone, restaurateurId: Id) {
+    this.id = id;
     this.name = name;
     this.phone = phone;
+    this.restaurateurId = restaurateurId;
   }
 
   public static create(props: EstablishmentProps): Either<PropsAreRequired | FieldIsRequired, Establishment> {
@@ -22,7 +30,11 @@ export class Establishment {
       return left(new PropsAreRequired());
     }
 
-    const { name, phone } = props;
+    const { id, name, phone, restaurateurId } = props;
+
+    if (!id) {
+      return left(new FieldIsRequired('id'));
+    }
 
     if (!name) {
       return left(new FieldIsRequired('name'));
@@ -32,7 +44,11 @@ export class Establishment {
       return left(new FieldIsRequired('phone'));
     }
 
-    const establishment = new Establishment(name, phone);
+    if (!restaurateurId) {
+      return left(new FieldIsRequired('restaurateurId'));
+    }
+
+    const establishment = new Establishment(id, name, phone, restaurateurId);
 
     return right(establishment);
   }
