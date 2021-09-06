b92dbb51a237b435b813457a2b5fb64c5d0ba54c
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-06 15:24:13 -0300

    feat: add create establishment usecase dto
    Story: [ch16]

--- packages/server/accounts/src/domain/use-cases/dtos/index.ts
+++ packages/server/accounts/src/domain/use-cases/dtos/index.ts
@@ -2,3 +2,4 @@ export * from '@/domain/use-cases/dtos/create-user';
 export * from '@/domain/use-cases/dtos/create-restaurateur';
 export * from '@/domain/use-cases/dtos/get-user';
 export * from '@/domain/use-cases/dtos/get-restaurateur';
+export * from '@/domain/use-cases/dtos/create-establishment';
