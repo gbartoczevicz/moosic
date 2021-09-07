3cdcb9360d93bf400f90769aeb33a9fdbc5064af
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-07 11:17:20 -0300

    feat: add artists repo
    Story: [ch13]

--- packages/server/accounts/src/ports/database/index.ts
+++ packages/server/accounts/src/ports/database/index.ts
@@ -1,3 +1,4 @@
 export * from '@/ports/database/users-repo';
 export * from '@/ports/database/restaurateur-repo';
 export * from '@/ports/database/establishments-repo';
+export * from '@/ports/database/artists-repo';
