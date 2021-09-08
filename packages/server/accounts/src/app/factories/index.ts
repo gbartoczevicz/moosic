8de7c9056669ac42a87dfe9c42ed62bf5a40c629
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-08 18:19:23 -0300

    feat: add create session controller factory
    Story: [ch42]

--- packages/server/accounts/src/app/factories/index.ts
+++ packages/server/accounts/src/app/factories/index.ts
@@ -2,3 +2,4 @@ export * from '@/app/factories/create-user.controller';
 export * from '@/app/factories/create-restaurateur.controller';
 export * from '@/app/factories/create-establishment.controller';
 export * from '@/app/factories/create-artist.controller';
+export * from '@/app/factories/create-session.controller';
