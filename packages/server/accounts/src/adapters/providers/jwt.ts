9a58e33a417d37366bca8b7fce899166db33f1f8
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-08 20:51:39 -0300

    chore: impl verify jwt provider
    Story: [ch42]

--- packages/server/accounts/src/adapters/providers/jwt.ts
+++ packages/server/accounts/src/adapters/providers/jwt.ts
@@ -5,4 +5,21 @@ export class JwtProviderImpl implements JwtProvider {
   public sign(payload: JwtSignPayload, secret: string, options: JwtSignOptions): string {
     return Provider.sign(payload, secret, options);
   }
+
+  /** @todo check if is working */
+  public verify(token: string, secret: string): string {
+    const { sub } = Provider.verify(token, secret);
+
+    if (!sub) {
+      throw new Error('Sub value not found');
+    }
+
+    console.log({ sub });
+
+    if (!(typeof sub === 'string')) {
+      throw new Error('Sub value is not a string');
+    }
+
+    return sub;
+  }
 }
