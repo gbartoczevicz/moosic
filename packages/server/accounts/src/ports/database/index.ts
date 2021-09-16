edb551416b14c6f68ff11fe56b3cdd7667e401c8
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-16 20:53:15 -0300

    chore: add LocationsRepo
    Story: [ch55]

--- packages/server/accounts/src/ports/database/index.ts
+++ packages/server/accounts/src/ports/database/index.ts
@@ -2,3 +2,4 @@ export * from '@/ports/database/users-repo';
 export * from '@/ports/database/restaurateur-repo';
 export * from '@/ports/database/establishments-repo';
 export * from '@/ports/database/artists-repo';
+export * from '@/ports/database/locations-repo';
