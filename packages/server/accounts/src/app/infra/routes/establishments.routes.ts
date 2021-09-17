c6bde8d4a5f284365cadf663a545004112b1ef20
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-17 20:13:33 -0300

    chore: add location route
    Story: [ch55]

--- packages/server/accounts/src/app/infra/routes/establishments.routes.ts
+++ packages/server/accounts/src/app/infra/routes/establishments.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
-import { createEstablishmentController } from '@/app/factories/controllers';
+import { createEstablishmentController, upsertLocationController } from '@/app/factories/controllers';
 
 const establishmentsRoutes = Router();
 
 establishmentsRoutes.post('/', async (request, response) => {
   const res = await createEstablishmentController.handle(request);
+  return response.status(res.statusCode).json(res.body);
+});
+
+establishmentsRoutes.post('/location', async (request, response) => {
+  const res = await upsertLocationController.handle(request);
   return response.status(res.statusCode).json(res.body);
 });
 
