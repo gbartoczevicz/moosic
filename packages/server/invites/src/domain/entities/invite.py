d516d2bbace0c3b128b413daa42c2b7b99bdd76f
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-23 19:16:44 -0300

    chore: add invite and comment entities
    Story: [sh67]

---
+++ packages/server/invites/src/domain/entities/invite.py
@@ -0,0 +1,21 @@
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel
+
+from .comment import Comment
+
+
+class Invite(BaseModel):
+  id: str
+  date: date
+  establishment_id: str
+  artist_id: str
+  latitude: float
+  longitude: float
+  postal_code: str
+  address: str
+  address_number: int
+  artist_rating: Optional[int]
+  establishment_rating: Optional[int]
+  artist_comment: Optional[Comment]
+  establishment_commnet: Optional[Comment]
