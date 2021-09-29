8c017f5b09398f54f83c03a0db1ba82cc6acf885
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-09-29 18:54:17 -0300

    feat: add signin page [sc-38]

---
+++ packages/mobile/src/pages/sign-in.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Title, Button, Container } from '@/lib';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Title>Bem-vindo</Title>
+    <Button>Entrar</Button>
+  </Container>
+);
+
+export default SignIn;
