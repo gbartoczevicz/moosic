4a19db375842d4bb59452a421ce1bab1faa220fd
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-11-06 15:28:46 -0300

    chore: update get enum value  [sc-74]

--- packages/server/accounts/src/utils/get-enum-by-value.ts
+++ packages/server/accounts/src/utils/get-enum-by-value.ts
@@ -1,9 +1,9 @@
-export function getEnumByValue<T>(anEnum: T, value: unknown): T[keyof T] | undefined {
-  const index = Object.values(anEnum).findIndex((val) => val === value);
+export function getEnumByValue<T>(anEnum: T, value: unknown): T | undefined {
+  const enumValues = Object.values(anEnum);
+
+  const index = enumValues.findIndex((val) => val === value);
 
   if (index === -1) return undefined;
 
-  const key = index as keyof T;
-
-  return anEnum[key];
+  return value as T;
 }
