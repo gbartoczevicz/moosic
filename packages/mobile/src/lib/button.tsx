35e75959e97814dfe0083dddeff3cf1b0d488660
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-11-10 21:26:13 -0300

    chore: update component props [sc-39]

--- packages/mobile/src/lib/button.tsx
+++ packages/mobile/src/lib/button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
-import { StyleProp, ViewStyle } from 'react-native';
+import { StyleProp, ViewProps, ViewStyle } from 'react-native';
 
 const light = '#fff';
 const dark = '#2f2e41';
@@ -27,25 +27,31 @@ const containerStyle: StyleProp<ViewStyle> = {
 };
 
 type Variant = 'dark' | 'light';
-type ButtonProps = RectButtonProps & { children: string; variant?: Variant };
+type ButtonProps = ViewProps &
+  Pick<RectButtonProps, 'onPress' | 'enabled'> & {
+    children: string;
+    variant?: Variant;
+  };
 
 export const Button: React.FC<ButtonProps> = (props) => {
   const { children, variant = 'light', ...delegate } = props;
+
+  const { onPress, enabled } = delegate;
 
   const containerBackgroundColor = variant === 'dark' ? dark : light;
   const labelFontColor = variant === 'dark' ? light : dark;
 
   const styleGuard = delegate.style ?? {};
-  const pressableStyle = (styleGuard as StyleProp<ViewStyle> as Array<unknown>)[0] as object;
+  const styleDelegate = (styleGuard as StyleProp<ViewStyle> as Array<unknown>)[0] as object;
 
   const borderStyling = variant === 'light' ? containerStyle : {};
 
   return (
-    <Container style={borderStyling}>
+    <Container style={{ ...styleDelegate, ...borderStyling }}>
       <Pressable
-        {...delegate}
+        onPress={onPress}
+        enabled={enabled}
         style={{
-          ...pressableStyle,
           backgroundColor: containerBackgroundColor
         }}
       >
