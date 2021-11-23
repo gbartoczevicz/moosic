8f770bf53c18be443d2e5bce21eafd25334fbf26
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-11-23 19:08:37 -0300

    chore: update Schedules screen

--- packages/mobile/src/pages/schedules.tsx
+++ packages/mobile/src/pages/schedules.tsx
@@ -1,27 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "@react-native-community/datetimepicker";
 
 import * as Lib from "@/lib";
 
 const Schedules: React.FC = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [togglePicker, setTogglePicker] = useState(false);
 
   const handleDateChange = (_event: unknown, date?: Date) => {
-    console.info("The new date is", date);
-
     if (date) {
       setCurrentDate(date);
     }
   };
 
+  useEffect(() => {
+    (() => setTogglePicker(false));
+  }, []);
+
   return (
     <Lib.Container>
-      <Calendar
-        mode="date"
-        display="calendar"
-        value={currentDate}
-        onChange={handleDateChange}
-      />
+      <Lib.Text>
+        The current date selected is {currentDate.toISOString()}
+      </Lib.Text>
+      <Lib.Button onPress={() => setTogglePicker(!togglePicker)} variant="dark">
+        Book a new date
+      </Lib.Button>
+      {togglePicker && (
+        <Calendar
+          mode="date"
+          display="calendar"
+          value={currentDate}
+          onChange={handleDateChange}
+        />
+      )}
     </Lib.Container>
   );
 };
