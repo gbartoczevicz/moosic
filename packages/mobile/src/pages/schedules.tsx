8df9d51a260ae13528a2ceef5008c9e977e79b1b
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-11-23 18:54:15 -0300

    chore: add calendar

--- packages/mobile/src/pages/schedules.tsx
+++ packages/mobile/src/pages/schedules.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+import Calendar from "@react-native-community/datetimepicker";
 
-import { Container, Title } from '@/lib/container';
+import { Container } from "@/lib/container";
 
-const Schedules: React.FC = () => (
-  <Container>
-    <Title>Agendamentos</Title>
-  </Container>
-);
+const Schedules: React.FC = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const handleDateChange = (_event: unknown, date?: Date) => {
+    console.info("The new date is", date);
+
+    if (date) {
+      setCurrentDate(date);
+    }
+  };
+
+  return (
+    <Container>
+      <Calendar
+        mode="date"
+        display="calendar"
+        value={currentDate}
+        onChange={handleDateChange}
+      />
+    </Container>
+  );
+};
 
 export default Schedules;
