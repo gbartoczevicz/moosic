9453567153c5664af802264b1933483179b5d3f0
Luiz Eduardo <luiz_1609@hotmail.com>
2021-11-24 00:02:50 -0300

    feat: list bands
    [sc-72]

--- packages/mobile/src/screens/profile.tsx
+++ packages/mobile/src/screens/profile.tsx
@@ -54,7 +54,7 @@ export const Profile: React.FC = () => {
           right: 25,
         }}
       >
-        <Avatar />
+        <Avatar url={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAOVBMVEV7H6L///9xAJzs4fH17PiBI6d6G6GDLqj38fmHPqraxOXy6fWFOamGO6n79PxuAJp5DqHUueDfy+grd0eUAAAA70lEQVR4nO3WTU7CABRGUahaf1BQ979YZ0jjwNakKRfPWcF3kzd4ux0AAAAAAAAAAAAAAAAAAPBPjBe23rKK8fX57O0mE4fD/uw4bL1mDcPdd+G9wiSFfQr7FPYp7FPYp7BPYZ/CPoV9CvsU9insU9insE9hn8I+hX0K+xT2KexT2KewT2Gfwj6FfQr7FPYp7FPYp7BPYZ/CPoV9CvsU9insmxSexnm2Hr3IpPD9caatVy9xWbh/muf4UTrnSeFcLwqviUKF10/hDRQefg/64bNUuHv4i61HLzPzF83+pQAAAAAAAAAAAAAAAAAAsKYvHCoPHMjDJaoAAAAASUVORK5CYII="} />
         <View
           style={{
             left: 30,
